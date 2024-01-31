9bf29739ea3e4dea4ef48f138bbf324a452f4fe6
jonas525 <jonas525@tutamail.com>
2024-01-31 23:14:23 +0100

    Finishing project ;)
    -Connect python fille to MariaDB database
    
    -Set up GUI in tkinter
    
    -Adding comments
    
    -Write README.me fille to project
    
    -Adding MIT License

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ExcuseGeneratorPy
-Excuse generator in python with MySQL
+
+ExcuseGeneratorPy is a simple Python application that generates random excuses. It uses Tkinter for the GUI and connects to a MariaDB database to fetch a list of excuses.
+
+## How to Use
+
+1. Make sure you have Python and MariaDB installed on your system.
+2. Create a MariaDB database named `Excuse`.
+3. Run the SQL script in the `excuse_database.sql` file to create the necessary table and populate it with excuses.
+4. Update the database connection details (host, user, password) in the Python script (`excuse_generator.py`).
+5. Run the Python script to launch the Excuse Generator.
+
+Feel free to customize the code according to your needs.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 import tkinter as tk
+import mysql.connector
+import random
 
+# Function to generate an excuse
+def generate_excuse():
+    random_excuse = random.choice(excuses)[0]
+    print("Wygenerowano wymówkę:", random_excuse, )
+    label_result.config(text=random_excuse, font=('Arial', 13))
+   
+# Database connection
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="Excuse"
+)
 
+cursor = connection.cursor()
+
+# Fetch excuses from the database
+cursor.execute("SELECT excuse_description FROM excuse_text")
+excuses = cursor.fetchall()
+
+#GUI setup
 root = tk.Tk()
-root.geometry("400x400")
+root.geometry("650x420")
 root.resizable(False, False)
+root.title("ExcuseGeneratorPy")
 
-
-
-label = tk.Label(root, text="ExcuseGeneratorPy", font=('Arial', 18))
+# Label for the title
+label = tk.Label(root, text="Excuse Generator", font=('Arial', 18))
 label.pack()
 
-button = tk.Button(root, text="Generate Excuse", font=('Arial', 18))
-button.pack(side=tk.BOTTOM, pady =(20,20))
+# Label to display the generated excuse
+label_result = tk.Label(root, text="")
+label_result.pack(pady=150)
 
+#Button to generate excuses
+button = tk.Button(root, text="Generate Excuse", font=('Arial', 20), command=generate_excuse)
+button.pack(side=tk.BOTTOM, pady =(20,10))
+
+#SQL database connection close
+connection.close()
+
+# Run the main event loop
 root.mainloop()
